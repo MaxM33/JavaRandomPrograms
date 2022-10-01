1456d80db3148c295b32bd8f933ff706847cf9c8
Massimo Monai <113710214+MaxM33@users.noreply.github.com>
2022-10-01 20:02:48 +0200

    Update Laboratory.java

--- Laboratory.java
+++ Laboratory.java
@@ -3,6 +3,28 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 // java Laboratory X Y Z (where X is number of Students, Y is number of Thesis worker, Z is number of Professors)
 
+/*
+	Il laboratorio è utilizzato da tre tipi di utenti (studenti, tesisti e professori) 
+	ed ogni utente deve fare una richiesta al tutor per accedere al laboratorio.
+	
+	I computer del laboratorio sono numerati da 1 a 20. Le richieste di accesso sono diverse a seconda del tipo dell'utente:
+	
+	- I professori accedono in modo esclusivo a tutto il laboratorio, poichè hanno necessità di utilizzare tutti i computer per effettuare prove in rete.
+	- I tesisti richiedono l'uso esclusivo di un solo computer, identificato dall'indice i, poiché su quel computer è installato 
+	  un particolare software necessario per lo sviluppo della tesi.
+	- Gli studenti richiedono l'uso esclusivo di un qualsiasi computer.
+
+	I professori hanno priorità su tutti nell'accesso al laboratorio, i tesisti hanno priorità sugli studenti.
+	Nessuno però può essere interrotto mentre sta usando un computer.
+
+	Il programma riceve in ingresso il numero di studenti, tesisti e professori che utilizzano il laboratorio ed attiva un thread per ogni utente.
+	Ogni utente accede k volte al laboratorio, con k generato casualmente.
+	L'intervallo di tempo che intercorre tra un accesso ed il successivo e l'intervallo di permanenza in laboratorio è simulato mediante il metodo sleep().
+	Il tutor coordina gli accessi al laboratorio.
+	Il programma termina quando tutti gli utenti hanno completato i loro accessi al laboratorio.
+	La logica di gestione del laboratorio è incapsulata all'interno di un monitor.
+*/
+
 class Computer {
 	int number;
 
